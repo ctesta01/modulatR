3013420f812fecac345c91e9d53dc4fb8b2ba65b
Christian Testa <ctesta@hsph.harvard.edu>
2025-10-18 10:51:35 -0400

    pkgdown setup; deleting files

--- R/lmtpi.R
+++
@@ -1,5 +0,0 @@
-
-# Add . and .data to the list of global variables so that
-# R CMD check --as-cran does not WARN that . and .data do not exist.
-utils::globalVariables(".")
-utils::globalVariables(".data")

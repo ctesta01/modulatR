b46ddb56782a765793e13a742cba6bff68cebc6e
Christian Testa <ctesta@hsph.harvard.edu>
2026-05-04 09:56:37 -0400

    syntax rewrite to more closely match Díaz et al 2023

--- R/run_tmle_for_LMTP.R
+++ R/run_tmle_for_LMTP.R
@@ -2,30 +2,28 @@
 
 #' Estimate a scalar TMLE for an LMTP mean
 #'
+#' @description
+#' Vector-based TMLE runner for
+#' \eqn{\psi = E[Y(\bar A^d)]}.
+#'
+#' All nuisance specifications live in `nuisance_factory`.
+#'
+#' The plug-in estimate uses targeted regressions `m_star`, while the estimated
+#' EIF used for variance uses untargeted regressions `m_init`.
+#'
 #' @param ds An `LMTPData` object.
-#' @param policy_seq An `LMTPPolicySequence`.
-#' @param nuisance_factory An `LMTPNuisanceFactory`.
-#' @param fluctuation An `LMTPFluctuationSubmodel`.
-#' @param learners_Q Learner spec passed to `q_trainer()`.
-#' @param fml_Q Optional outcome-regression formula/formulas.
-#' @param learners_Q_extra_args Optional extra args for Q learners.
-#' @param alpha Confidence level parameter.
+#' @param nuisance_factory An `LMTPNuisanceFactory` object.
+#' @param fluctuation An `LMTPFluctuationSubmodel` object.
+#' @param alpha Wald interval level.
 #'
 #' @return An `LMTPFit`.
 #' @export
 run_tmle_for_LMTP <- function(ds,
-                              policy_seq,
                               nuisance_factory,
-                              fluctuation,
-                              learners_Q,
-                              fml_Q = NULL,
-                              learners_Q_extra_args = NULL,
+                              fluctuation = LMTPFluctuationSubmodel$new(),
                               alpha = 0.05) {
   if (!inherits(ds, "LMTPData")) {
     stop("`ds` must inherit from `LMTPData`.")
-  }
-  if (!inherits(policy_seq, "LMTPPolicySequence")) {
-    stop("`policy_seq` must inherit from `LMTPPolicySequence`.")
   }
   if (!inherits(nuisance_factory, "LMTPNuisanceFactory")) {
     stop("`nuisance_factory` must inherit from `LMTPNuisanceFactory`.")
@@ -34,146 +32,170 @@ run_tmle_for_LMTP <- function(ds,
     stop("`fluctuation` must inherit from `LMTPFluctuationSubmodel`.")
   }
 
+  policy_seq <- nuisance_factory$policy_seq
+  if (!inherits(policy_seq, "LMTPPolicySequence")) {
+    stop("`nuisance_factory$policy_seq` must inherit from `LMTPPolicySequence`.")
+  }
+
   policy_seq$validate_against_data(ds)
-  nuisance_factory$train(ds)
-  kprov <- nuisance_factory$k_provider(ds)
 
   tau <- ds$tau()
+  n <- ds$n
 
-  q_fit_factory <- nuisance_factory$q_trainer(
-    learners_Q = learners_Q,
-    fml_Q = fml_Q,
-    learners_Q_extra_args = learners_Q_extra_args
-  )
+  # -----------------------------------------------------------------------
+  # 1. Fit ratio nuisance vectors.
+  #
+  # Stores:
+  #   r_obs[[t]]     = r_t(A_t, H_t)
+  #   r_d[[t]]       = r_t(A_t^d, H_t)
+  #   omega_obs[[t]] = prod_{s <= t} r_s(A_s, H_s)
+  # -----------------------------------------------------------------------
 
-  # start of backward recursions --------------------------------------------
+  nuisance_factory$train(ds)
 
-  Q_init <- vector("list", tau)
-  Q_star <- vector("list", tau)
-  eps <- vector("list", tau)
-  intercept <- vector("list", tau)
+  # -----------------------------------------------------------------------
+  # 2. Fit initial, untargeted sequential regressions.
+  #
+  # Stores:
+  #   m_init_obs[[t]] = m_t(A_t, H_t)
+  #   m_init_d[[t]]   = m_t(A_t^d, H_t)
+  #
+  # The pseudo-outcome recursion is:
+  #   pseudo_tau = Y
+  #   pseudo_{t-1} = m_t(A_t^d, H_t)
+  # -----------------------------------------------------------------------
 
-  # -----------------------------
-  # Track 1: untargeted recursion
-  # -----------------------------
-  target_next <- ds$Y()
+  m_init_obs <- vector("list", tau)
+  m_init_d <- vector("list", tau)
+
+  pseudo_outcome_init <- ds$Y()
 
   for (t in rev(seq_len(tau))) {
-    A_t <- ds$A(t)
-    H_t <- ds$H(t)
-    A_t_star <- policy_seq$apply_t(t, A_t, H_t)
+    fit_m_t <- nuisance_factory$fit_m(
+      data = ds,
+      t = t,
+      pseudo_outcome = pseudo_outcome_init
+    )
 
-    Q0_t <- q_fit_factory(ds, t = t, pseudo_outcome_vec = target_next)
-
-    Q_init[[t]] <- Q0_t
+    m_init_obs[[t]] <- fit_m_t$m_obs
+    m_init_d[[t]] <- fit_m_t$m_d
 
     if (t > 1L) {
-      target_next <- Q_init[[t]](A_t_star, H_t)
+      pseudo_outcome_init <- fit_m_t$m_d
     }
   }
 
-  # ----------------------------------------
-  # Track 2: targeted recursion for TMLE psi
-  # ----------------------------------------
-  target_next_star <- ds$Y()
+  # -----------------------------------------------------------------------
+  # 3. Targeted backward recursion.
+  #
+  # The fluctuation object handles the submodel. The runner supplies vectors:
+  #
+  #   observed clever covariate:
+  #     H_obs_t = omega_t
+  #
+  #   policy-evaluation clever covariate:
+  #     H_d_t = omega_{t-1} * r_t(A_t^d, H_t)
+  #
+  # The plug-in recursion uses targeted policy-value predictions m_t^d,*.
+  # -----------------------------------------------------------------------
+
+  m_star_obs <- vector("list", tau)
+  m_star_d <- vector("list", tau)
+  eps <- vector("list", tau)
+  intercept <- vector("list", tau)
+  fluctuation_fits <- vector("list", tau)
+
+  pseudo_outcome_star <- ds$Y()
 
   for (t in rev(seq_len(tau))) {
-    A_t <- ds$A(t)
-    H_t <- ds$H(t)
-    A_t_star <- policy_seq$apply_t(t, A_t, H_t)
+    H_obs_t <- nuisance_factory$omega(t)
+    H_d_t <- nuisance_factory$omega_prev(t, n = n) * nuisance_factory$r_d[[t]]
 
-    Q0_t <- Q_init[[t]]
-    Qvec_t <- Q0_t(A_t, H_t)
-
-    H_obs_t <- kprov$K_obs(t)
-
-    K_prev_obs <- if (t == 1L) rep(1, ds$n) else kprov$K_obs(t - 1L)
-
-    r_t_fun <- nuisance_factory$r_list[[t]]
-    if (is.null(r_t_fun) || !is.function(r_t_fun)) {
-      stop("`r_t_fun` is NULL or not a function at time t = ", t, ".")
-    }
-
-    H_fun_t <- local({
-      K_prev_obs_local <- K_prev_obs
-      r_t_fun_local <- r_t_fun
-      function(A_vec, H_df) {
-        K_prev_obs_local * r_t_fun_local(A_vec, H_df)
-      }
-    })
-
-    up <- fluctuation$fit_update(
-      Q0_fun_t = Q0_t,
-      Qvec = Qvec_t,
-      target_vec = target_next_star,
+    update_t <- fluctuation$fit_update(
+      m_obs = m_init_obs[[t]],
+      target = pseudo_outcome_star,
       H_obs = H_obs_t,
-      H_fun_t = H_fun_t,
+      m_d = m_init_d[[t]],
+      H_d = H_d_t,
       t = t
     )
 
-    Q_star[[t]] <- up$wrap
-    eps[[t]] <- up$epsilon
-    intercept[[t]] <- up$intercept
+    m_star_obs[[t]] <- update_t$m_obs_star
+    m_star_d[[t]] <- update_t$m_d_star
+    eps[[t]] <- update_t$epsilon
+    intercept[[t]] <- update_t$intercept
+    fluctuation_fits[[t]] <- update_t$fit
 
     if (t > 1L) {
-      target_next_star <- Q_star[[t]](A_t_star, H_t)
+      pseudo_outcome_star <- update_t$m_d_star
     }
   }
 
-  # Plug-in estimate
-  A1_star <- policy_seq$apply_t(1, ds$A(1), ds$H(1))
+  # -----------------------------------------------------------------------
+  # 4. TMLE plug-in estimate.
+  #
+  # Uses targeted m_1 evaluated at the modified treatment value.
+  # -----------------------------------------------------------------------
 
-  psi_tmle <- mean(Q_star[[1]](A1_star, ds$H(1)))
-  psi_plugin_init <- mean(Q_init[[1]](A1_star, ds$H(1)))
+  psi_hat <- mean(m_star_d[[1L]])
 
-  # EIF matching the parameterization:
+  # -----------------------------------------------------------------------
+  # 5. Estimated EIF / IC.
   #
-  # for t < tau:
-  #   K_t { Q_{t+1}(A_{t+1}^d, H_{t+1}) - Q_t(A_t, H_t) }
+  # Crucial: use untargeted nuisance regressions m_init, not m_star.
   #
-  # for t = tau:
-  #   K_tau { Y - Q_tau(A_tau, H_tau) }
+  #   D_hat(O_i)
+  #     = sum_t omega_t(O_i)
+  #         { next_untargeted_t(O_i) - m_t(A_t,H_t)(O_i) }
+  #       + m_1(A_1^d,H_1)(O_i) - psi_hat
   #
-  # plus plug-in term:
-  #   Q_1(A_1^d, H_1) - psi
-  ic <- rep(0, ds$n)
+  # where:
+  #   next_untargeted_t = Y for t = tau
+  #   next_untargeted_t = m_{t+1}(A_{t+1}^d,H_{t+1}) otherwise.
+  # -----------------------------------------------------------------------
+
+  eif <- rep(0, n)
 
   for (t in seq_len(tau)) {
-    K_t <- kprov$K_obs(t)
-
-    if (t < tau) {
-      A_next_star <- policy_seq$apply_t(t + 1L, ds$A(t + 1L), ds$H(t + 1L))
-
-      Q_next_init_shift <- Q_init[[t + 1L]](A_next_star, ds$H(t + 1L))
-      Q_t_init_obs <- Q_init[[t]](ds$A(t), ds$H(t))
-
-      ic <- ic + K_t * (Q_next_init_shift - Q_t_init_obs)
+    next_term_untargeted <- if (t == tau) {
+      ds$Y()
     } else {
-      Q_t_init_obs <- Q_init[[t]](ds$A(t), ds$H(t))
-      ic <- ic + K_t * (ds$Y() - Q_t_init_obs)
+      m_init_d[[t + 1L]]
     }
+
+    eif <- eif +
+      nuisance_factory$omega(t) *
+      (next_term_untargeted - m_init_obs[[t]])
   }
 
-  ic <- ic + Q_init[[1]](A1_star, ds$H(1)) - psi_plugin_init
+  eif <- eif + m_init_d[[1L]] - psi_hat
 
-  var_hat <- stats::var(ic) / ds$n
+  var_hat <- stats::var(eif) / n
   se_hat <- sqrt(var_hat)
-  ci_hat <- .wald_ci(psi_tmle, se_hat, alpha = alpha)
+  ci <- .wald_ci(psi_hat, se_hat, alpha = alpha)
 
   LMTPFit$new(
-    estimate = psi_tmle,
+    estimate = psi_hat,
     var = var_hat,
     se = se_hat,
-    ci = ci_hat,
+    ci = ci,
     alpha = alpha,
     parameter = "E[Y^d]",
-    estimator = "LMTP TMLE",
-    n = ds$n,
-    ic = ic,
-    eif = ic,
-    Q_init = Q_init,
-    Q_star = Q_star,
-    omega = kprov$K_obs_all,
+    estimator = "TMLE",
+    n = n,
+    ic = eif,
+    eif = eif,
+
+    Q_init = list(
+      m_obs = m_init_obs,
+      m_d = m_init_d
+    ),
+    Q_star = list(
+      m_obs = m_star_obs,
+      m_d = m_star_d
+    ),
+
+    omega = nuisance_factory$omega_obs,
     eps = eps,
     intercept = intercept
   )
